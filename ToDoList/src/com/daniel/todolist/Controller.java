aa7008e8a531a7beb03820bf8af3c67ed10bf9ce
Daniel <100775726+tenczowy@users.noreply.github.com>
2022-03-02 13:03:14 +0000

    Upload Files

---
+++ ToDoList/src/com/daniel/todolist/Controller.java
@@ -0,0 +1,148 @@
+package com.daniel.todolist;
+
+import com.daniel.todolist.datamodel.ToDoData;
+import com.daniel.todolist.datamodel.ToDoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.util.Callback;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+public class Controller {
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    @FXML
+    private Label deadlineLabel;
+
+    private List<ToDoItem> toDoItems;
+    @FXML
+    private ListView<ToDoItem> todoListView;
+    @FXML
+    private BorderPane mainBorderPane;
+
+
+    public void initialize(){
+//        ToDoItem item1 = new ToDoItem("Spotkanie Biznesowe", "Spotkanie z Januszem w sprawie pracy",
+//                LocalDate.of(2022, Month.APRIL, 04));
+//        ToDoItem item2 = new ToDoItem("Impreza urodzinowa", "Urodziny kasi, kupić kwiaty",
+//                LocalDate.of(2022, Month.DECEMBER, 21));
+//        ToDoItem item3 = new ToDoItem("Kupić mleko", "Wyjść z domu, kupić mleko, ale dobre",
+//                LocalDate.of(2022, Month.FEBRUARY, 28));
+//
+//        toDoItems = new ArrayList<ToDoItem>();
+//        toDoItems.add(item1);
+//        toDoItems.add(item2);
+//        toDoItems.add(item3);
+//
+//        ToDoData.getInstance().setToDoItems(toDoItems);
+
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<ToDoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends ToDoItem> observableValue, ToDoItem toDoItem, ToDoItem t1) {
+                if(t1 != null){
+                    ToDoItem item = todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("d MMMM, yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadline()));
+                }
+            }
+        });
+
+
+        todoListView.setItems(ToDoData.getInstance().getToDoItems());
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
+
+        todoListView.setCellFactory(new Callback<ListView<ToDoItem>, ListCell<ToDoItem>>() {
+            @Override
+            public ListCell<ToDoItem> call(ListView<ToDoItem> toDoItemListView) {
+                ListCell<ToDoItem> cell = new ListCell<ToDoItem>(){
+                    @Override
+                    protected void updateItem(ToDoItem toDoItem, boolean empty) {
+                        super.updateItem(toDoItem, empty);
+                        if(empty){
+                            setText(null);
+                        }else {
+                            setText(toDoItem.getShortDescription());
+                            if(toDoItem.getDeadline().isBefore(LocalDate.now().plusDays(1))){
+                                setTextFill(Color.RED);
+                            }else if(toDoItem.getDeadline().equals(LocalDate.now().plusDays(1))){
+                                setTextFill(Color.BLUE);
+                            }
+                        }
+                    }
+                };
+                return cell;
+            }
+        });
+    }
+
+    @FXML
+    public void showNewItemDialog(){
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        dialog.setTitle("Add new TuDu Item");
+        dialog.setHeaderText("Use this dialog to create new TuDu Item");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("todoItemDialog.fxml"));
+        try{
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        }catch (IOException e){
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+        Optional<ButtonType> result = dialog.showAndWait();
+
+        if(result.isPresent() && result.get() == ButtonType.OK){
+            DialogController controller = fxmlLoader.getController();
+            ToDoItem newItem = controller.processResults();
+           //todoListView.getItems().setAll(ToDoData.getInstance().getToDoItems());
+            todoListView.getSelectionModel().select(newItem);
+    }
+
+//    @FXML
+//    public void handleClickListView(){
+//
+//        ToDoItem item = todoListView.getSelectionModel().getSelectedItem();
+//        itemDetailsTextArea.setText(item.getDetails());
+//        deadlineLabel.setText(item.getDeadline().toString());
+//
+//
+//        StringBuilder sb = new StringBuilder(item.getDetails());
+//        sb.append("\n\n\n\n");
+//        sb.append("Due: ");
+//        sb.append(item.getDeadline().toString());
+//        itemDetailsTextArea.setText(sb.toString());
+//    }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ToDoList/src/com/daniel/todolist/DialogController.java
@@ -0,0 +1,30 @@
+package com.daniel.todolist;
+
+import com.daniel.todolist.datamodel.ToDoData;
+import com.daniel.todolist.datamodel.ToDoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+public class DialogController {
+    @FXML
+    private TextField shortDescriptionField;
+    @FXML
+    private TextArea detailsArea;
+    @FXML
+    private DatePicker deadLinePicker;
+
+    public ToDoItem processResults(){
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadLineValue = deadLinePicker.getValue();
+
+
+        ToDoItem newItem = new ToDoItem(shortDescription,details,deadLineValue);
+        ToDoData.getInstance().addToDoItem(newItem);
+        return newItem;
+    }
+}
